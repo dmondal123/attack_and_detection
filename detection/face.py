8b6dfc93c9c161b0ad85ebee0645e9ca7d0b6720
dmondal123 <156932471+dmondal123@users.noreply.github.com>
2024-07-07 21:42:46 +0530

    test

--- detection/face.py
+++ detection/face.py
@@ -53,53 +53,7 @@ openai_function = {
     }
   }
 
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
 
-def draw_clown_face(face_color='yellow', eye_color='black', 
-                    nose_color='red'):
-    """
-    Draws a customizable, simplified clown face using matplotlib.
-
-    Parameters:
-    - face_color (str): Color of the clown's face. Default is 'yellow'.
-    - eye_color (str): Color of the clown's eyes. Default is 'black'.
-    - nose_color (str): Color of the clown's nose. Default is 'red'.
-
-    This function creates a plot displaying a simplified clown face, where essential facial features' size, position, and color can be customized. 
-    """
-    # Constants
-    face_radius = 0.4
-    nose_radius = 0.1
-    nose_x, nose_y = 0.5, 0.5
-    mouth_x, mouth_y = 0.5, 0.3
-    mouth_color = 'black'
-    eye_size = 0.05
-    mouth_size = (0.3, 0.1)
-    eye_offset=(0.15, 0.15)
-    mouth_theta = (200, 340)
-
-    fig, ax = plt.subplots()
-    # Face
-    face = patches.Circle((0.5, 0.5), face_radius, color=face_color, fill=True)
-    ax.add_patch(face)
-    # Eyes
-    eye_left = patches.Circle((0.5-eye_offset[0], 0.5+eye_offset[1]), eye_size, color=eye_color, fill=True)
-    eye_right = patches.Circle((0.5+eye_offset[0], 0.5+eye_offset[1]), eye_size, color=eye_color, fill=True)
-    ax.add_patch(eye_left)
-    ax.add_patch(eye_right)
-    # Nose
-    nose = patches.Circle((nose_x, nose_y), nose_radius, color=nose_color, fill=True)
-    ax.add_patch(nose)
-    # Mouth
-    mouth = patches.Arc((mouth_x, mouth_y), mouth_size[0], mouth_size[1], angle=0, 
-                        theta1=mouth_theta[0], theta2=mouth_theta[1], color=mouth_color, linewidth=2)
-    ax.add_patch(mouth)
-    # Setting aspect ratio to 'equal' to ensure the face is circular
-    ax.set_aspect('equal')
-    # Remove axes
-    ax.axis('off')
-    plt.show()
 
 openai_msg = \
 "Hey can you draw a pink clown face with a red nose"
